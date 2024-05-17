7fa6308479117a01a32bbbdce8b312f598ef651b
Wendymackennah <wendymackennah5@gmail.com>
2024-05-17 10:35:18 +0300

    navbar

--- farmart_app/src/components/NavBar.js
+++ farmart_app/src/components/NavBar.js
@@ -1,28 +1,33 @@
-import React from 'react'
+
+import React from "react";
+import "./NavBar.css";
 import { Link } from "react-router-dom"; 
 
-const NavBar = () => {
-    return (
-        <nav className="navbar">
-          <div className="navbar-logo">
-            <Link to="/">Farmart</Link>
-          </div>
-          <ul className="navbar-links">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/">Dashboard</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
-      );
-    };
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <Link to="/">Farmart</Link>
+      </div>
+      <ul className="navbar-links">
+        <li>
+          <Link to="/signup">SignUp</Link>
+        </li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/products">Products</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-export default NavBar
+export default Navbar;
