3db4199af6451c4763a08a594b90002e642de5f4
Wanjiru Mburu <wanjirumarionne@gmail.com>
2024-05-18 13:28:44 +0300

    Adjusted home styling and added json file for fetch testing

--- farmart_app/src/components/Home.js
+++ farmart_app/src/components/Home.js
@@ -1,55 +1,49 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import "./Home.css"
 import Sidebar from './Sidebar'
 import Footer from './Footer'
 import { CartContext } from './MyCartContext';
+import HomeSliders from './HomeSliders';
 
-const Home = () => {
-  const animals = [
-    { id: 1, breed: 'Goat', category: 'livestock', image_url: 'https://www.africanfarming.com/wp-content/uploads/komatsu.jpeg', price: 1000, farmer_id: 'seller1' },
-    { id: 3, breed: 'Horse', category: 'equines', image_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTImaFZP_SRMx0vtv_qz65a1coTjlgaM6YLgJXUJciV9Q&s', price: 2000, farmer_id: 'seller2' },
-    { id: 1, breed: 'Goat', category: 'livestock', image_url: 'https://www.africanfarming.com/wp-content/uploads/komatsu.jpeg', price: 1000, farmer_id: 'seller1' },
-    { id: 3, breed: 'Horse', category: 'equines', image_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTImaFZP_SRMx0vtv_qz65a1coTjlgaM6YLgJXUJciV9Q&s', price: 2000, farmer_id: 'seller2' },
-  ];
+function Home () {
   /*Setting states for loading the DOM, all animals and selecting categories*/
-  ///const [isError, setIsError] = useState(false);
-  ///const [isLoaded, setIsLoaded] = useState(false);
-  ///const [allAnimals, setAllAnimals] = useState([]);
+  const [isError, setIsError] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [allAnimals, setAllAnimals] = useState([]);
   ///const [allAnimals, setAllAnimals] = useState(animals);
   const [filteredAnimals, setFilteredAnimals] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [showAnimalDetails, setShowAnimalDetails] = useState(false);
   const { addToCart } = useContext(CartContext);
   const [searchTerm, setSearchTerm] = useState("");
+  
 
   /*Funtion to fetch all animals when the DOM loads*/
-  ///useEffect (() => {
-    ///fetch ("/farm_animals")
-    ///.then ((response) => response.json())
-    ///.then ((data) => {
+  useEffect (() => {
+    fetch ("/data.json")
+    .then ((response) => response.json())
+    .then ((data) => {
         /*After fetching the data, the animals are loaded, and the isLoaded state changes to true*/
-        ///setIsLoaded(true);
+        setIsLoaded(true);
         /*Sets the animals state with the fetched data */
-        ///setAllAnimals(data);
-        ///setFilteredAnimals(data);
-    ///})
+        setAllAnimals(data.farmart);
+        setFilteredAnimals(data.farmart);
+    })
     /*error handling that sets the isError state */
-    ///.catch((error) => {
-        ///setIsError(true);
-        ///console.error('Error loading animals:', error);
-    ///});
+    .catch((error) => {
+        setIsError(true);
+        console.error('Error loading animals:', error);
+    });
     
-  ///}, []); /*There are no dependencies for the useEffect as it will be rendered only after loading, hence an empty array */
+  }, []); /*There are no dependencies for the useEffect as it will be rendered only after loading, hence an empty array */
 
   /*Funcion to filter the animals when state changes for the selectedCategory*/
   function filterAnimalsByCategory(category) {
-    if (category === null) {
-      setFilteredAnimals(animals);
-      ///setFilteredAnimals(allAnimals);
-    } else if (category === 'all-animals') {
-      setFilteredAnimals(animals);
+    if (category === null || category === 'all-animals') {
+      ///setFilteredAnimals(animals);
+      setFilteredAnimals(allAnimals);
     } else {
-    const animalsFiltered = animals.filter(animal => animal.category === category);
+    const animalsFiltered = allAnimals.filter(animal => animal.category.toLowerCase() === category.toLowerCase());
     setFilteredAnimals(animalsFiltered)
     }
   }
@@ -58,40 +52,42 @@ const Home = () => {
   function handleCategoryClick (category) {
     if (category === selectedCategory) {
       return; 
-    } else if (category === null) {
-      return filteredAnimals
     }
     setSelectedCategory(category);
     filterAnimalsByCategory(category);
   };
 
+  /*Function to toggele animal details*/
   function toggleDetails () {
     setShowAnimalDetails(!showAnimalDetails);
   };
 
   /*Function to filter animals by search term*/
   function handleSearchChange(event) {
-  const searchTerm = event.target.value.toLowerCase();
-  setSearchTerm(searchTerm);
-  const animalsFiltered = animals.filter(animal => 
-    animal.breed.toLowerCase().includes(searchTerm) || 
-    animal.category.toLowerCase().includes(searchTerm)
-  );
-  setFilteredAnimals(animalsFiltered);
+    const searchTerm = event.target.value.toLowerCase();
+    setSearchTerm(searchTerm);
+    const animalsFiltered = allAnimals.filter(animal => 
+      animal.breed.toLowerCase().includes(searchTerm) || 
+      animal.category.toLowerCase().includes(searchTerm) ||
+      animal.type.toLowerCase().includes(searchTerm)
+    );
+    setFilteredAnimals(animalsFiltered);
   }
 
 
   /*Renders if there is an error with DOM loading */
-  ///if (isError) {
-    ///return <div>Error occurred while loading data</div>;
-  ///}
+  if (isError) {
+    return <div>Error occurred while loading data</div>;
+  }
 
   /*Renders when the DOM loads but the animals are not fetched */
-  ///if (!isLoaded) {
-    ///return <div>Loading...</div>;
-  ///}
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
+
   return (
       <div>
+        < HomeSliders />
         <div className='home-sidebar'>
           < Sidebar />
           <div className='home-container'>
@@ -126,7 +122,12 @@ const Home = () => {
                     <img className='images' src={animal.image_url} alt={animal.name} />
                   </div>
                   <div className='animal-details'>
-                    {animal.breed} - ${animal.price}
+                    {animal.breed} - Kes. {animal.price}
+                  </div>
+                  <div>
+                    <button className="add-cart-button" onClick={() => addToCart(animal)}>
+                      Add to Cart
+                    </button>
                   </div>
                   <div className="additional-animal-details">
                     {showAnimalDetails && (
@@ -135,13 +136,10 @@ const Home = () => {
                         <p>Type: {animal.type}</p>
                         <p>Age: {animal.age}</p>
                         <p>Weight: {animal.weight}</p>
-                        <button className="button" onClick={() => addToCart(animal)}>
-                          Add to Cart
-                        </button>
                         <p>Description: {animal.description}</p>
                         </>
                     )}
-                    <button className= "button" onClick={toggleDetails}>
+                    <button className= "button-showdetails" onClick={toggleDetails}>
                         {showAnimalDetails ? 'Hide Profile' : 'Show Animal Profile'}
                     </button>
                   </div>
