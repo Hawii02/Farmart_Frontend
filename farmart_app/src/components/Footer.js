b05bc0b7aef6ece854a232da2d6c935b9db6a93c
leon <onditileon21@gmail.com>
2024-05-30 12:08:45 +0300

    Incoming changes

--- farmart_app/src/components/Footer.js
+++ farmart_app/src/components/Footer.js
@@ -58,6 +58,9 @@
 import React from "react";
 import { Link } from "react-router-dom"; 
 import "./Footer.css";
+import XIcon from '@mui/icons-material/X';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 const Footer = () => {
   return (
@@ -67,13 +70,7 @@ const Footer = () => {
           <h4>Quick Links</h4>
           <ul>
             <li>
-            <Link to="/signup">Sign Up</Link>
-            </li>
-            <li>
               <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/products">Products</Link>
             </li>
             <li>
               <Link to="/aboutus">About Us</Link>
@@ -85,12 +82,13 @@ const Footer = () => {
         </div>
         <div className="footer-section">
           <h4>Connect With Us</h4>
+          <div className="footer-icons">
           <ul>
-            <li>Twitter</li>
-            <li>Facebook</li>
-            <li>Instagram</li>
-            <li>Tiktok</li>
+            <li>Twitter <XIcon /></li>
+            <li>Facebook <FacebookIcon/></li>
+            <li>Instagram <InstagramIcon /></li>
           </ul>
+          </div>
         </div>
         <div className="footer-section">
           <h4>Contact Us</h4>
